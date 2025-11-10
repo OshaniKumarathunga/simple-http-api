e4a48ddf93a19e100679edb0745217f0acc3e835
Oshani Kumarathunga <100805282+OshaniKumarathunga@users.noreply.github.com>
2025-11-10 13:17:47 +0530

    Initial commit

---
+++ src/main/java/com/example/simple_http_api/SimpleHttpApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.simple_http_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleHttpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleHttpApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simple_http_api/SimpleHttpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simple_http_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleHttpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
